47150b8be19c84b7fe2174e0c2723b30cdcb9b2d
danny ba <dannba0@gmail.com>
2022-10-29 12:02:09 +0100

    all sm done

--- contracts/Switcher.sol
+++ contracts/Switcher.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IEulerEToken, IEulerMarkets, EulerAddrGoerli, IEulerDToken } from "./interfaces/IEuler.sol";
@@ -8,7 +8,7 @@ import { IEulerEToken, IEulerMarkets, EulerAddrGoerli, IEulerDToken } from "./in
 contract Switcher {
     
     address public collateralToken = address(0x693FaeC006aeBCAE7849141a2ea60c6dd8097E25); // USDC
-    address public EULER_GOERLI = address(0x931172BB95549d0f29e10ae2D079ABA3C63318B3); // Euler Goerli 
+    address public EULER_TESTNET = address(0x931172BB95549d0f29e10ae2D079ABA3C63318B3); // Euler Goerli 
     address public borrowedToken = address(0xa3401DFdBd584E918f59fD1C3a558467E373DacC); // WETH
 
     IEulerMarkets markets = IEulerMarkets(0x3EbC39b84B1F856fAFE9803A9e1Eae7Da016Da36);
@@ -22,7 +22,7 @@ contract Switcher {
     event Withdrawal(uint256 amount, uint256 when);
     event Deposit(uint256 amount);
     event VerifySettings(address token, uint256 healthRatio);
-    event Adjustment(address _beneficiary, address _owner, uint256 _amount);
+    event Rebalance(address _beneficiary, address _owner, uint256 _amount);
     event Management(address _beneficiary, address _owner, uint256 _healthRatio);
 
     constructor() payable {
@@ -42,7 +42,7 @@ contract Switcher {
 
         // Approve Euler Goerli contract to spend USDC
         IERC20(collateralToken).approve(
-            EULER_GOERLI,
+            EULER_TESTNET,
             type(uint).max
         );
 
@@ -67,53 +67,63 @@ contract Switcher {
         // Error I get is "e/insufficient-tokens-available"
     }
 
-    // function manage(uint256 newHealthRatio) public {
-
-    //     // get the current health ratio
-    //     uint256 currentHealthRatio = healthRatios[msg.sender];
-
-    //     // get the current balance
-    //     uint256 currentBalance = balances[msg.sender];
-
-    //     // if the new health ratio is greater than the current health ratio
-    //     if (newHealthRatio > currentHealthRatio) {
-
-    //         // get the amount of tokens to be deposited or withdrawn
-    //         uint256 amount = (newHealthRatio - currentHealthRatio) * currentBalance;
-
-    //         // withdraw tokens from sommelier 
-
-
-    //         // deposit the amount of tokens into euler
-    //         eToken.deposit(0, amount);
-    //         // borrow the amount of tokens
-
-    //         borrowedDToken.borrow(0, amount);
-
-    //     } else {
-
-    //         // get the amount of tokens to be deposited or withdrawn
-    //         uint256 amount = (currentHealthRatio - newHealthRatio) * currentBalance;
+    function manage(uint256 newHealthRatio) public {
+        // get the current health ratio
+        uint256 currentHealthRatio = healthRatios[msg.sender];
+        
+        // get the current balance
+        uint256 currentBalance = balances[msg.sender];
+        
+        // if the new health ratio is greater than the current health ratio
+        if (newHealthRatio > currentHealthRatio) {
+            // get the amount of tokens to be deposited or withdrawn
+            uint256 amount = (newHealthRatio - currentHealthRatio) * currentBalance;
+            // withdraw tokens from sommelier
             
-    //         // approve & repay the amount of tokens
-    //         IERC20(borrowedToken).approve(EULER_TESTNET, type(uint).max);
-    //         borrowedDToken.repay(0, amount);
+            // deposit the amount of tokens into euler
+            eToken.deposit(0, amount);
+            // borrow the amount of token
+            borrowedDToken.borrow(0, amount);
 
-    //         // withdraw the amount of tokens
-    //         eToken.withdraw(0, amount);
+        } else {
+            // get the amount of tokens to be deposited or withdrawn
+            uint256 amount = (currentHealthRatio - newHealthRatio) * currentBalance;
 
-    //         // supply back into sommelier
+            // approve & repay the amount of tokens
+            IERC20(borrowedToken).approve(EULER_TESTNET, type(uint).max);
+            //repay the amount of tokens
+            borrowedDToken.repay(0, amount);
+            // withdraw the amount of tokens
+            eToken.withdraw(0, amount);
+            // supply back into sommelie
+        }
+            
+        // update the health ratio
+        healthRatios[msg.sender] = newHealthRatio;
+        emit Management(msg.sender, owner, newHealthRatio);
+    }
 
-    //     }
+    
+    function rebalance() public {
+        // if the health ratio is greater than 5% 
+        // withdraw from euler
+        uint256 eamount; 
 
-    //     // update the health ratio
-    //     healthRatios[msg.sender] = newHealthRatio;
+        //repay the amount of tokens
+        borrowedDToken.repay(0, eamount);
 
-    //     emit Management(msg.sender, owner, newHealthRatio);
+        // withdraw the amount of tokens
+        eToken.withdraw(0, eamount);
+        
+        //make  the current health ratio = target health ratio from Euler through calculations
+        // deposit into sommelier
 
-    // }
-
-    function adjustment() public {
+        // if the health ratio is less than 5%
+        // withdraw from sommelier
+        //make  the current health ratio = target health ratio from Euler through calculations
+        uint256 samount; 
+        // deposit into euler
+        eToken.deposit(0, samount);
 
     }
 
@@ -121,7 +131,7 @@ contract Switcher {
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
-        IERC20(borrowedToken).approve(EULER_GOERLI, type(uint).max);
+        IERC20(borrowedToken).approve(EULER_TESTNET, type(uint).max);
         borrowedDToken.repay(0, type(uint).max);
 
         eToken.withdraw(0, amountToWithdraw);
--- test/Switcher.js
+++ test/Switcher.js
@@ -2,17 +2,18 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 
 describe("test contract", function () {
-    it("Deployment should have a beneficiary", async function () {
+    it("Deployment should have a beneficiary and deposit", async function () {
         const [owner] = await ethers.getSigners();
-        const tokenvalue = hre.ethers.utils.parseEther("1");
+        const tokenvalue = hre.ethers.utils.parseEther("100");
+        const tokencol = hre.ethers.utils.parseEther("10");
+        const tokenloan = hre.ethers.utils.parseEther("40");
 
         const Manage = await ethers.getContractFactory("Switcher");
+        const manage = await Manage.deploy();
 
-        const manage = await Manage.deploy(owner.address);
+        //await manage.deployed();
 
-        await manage.deployed();
-
-
-        await manage.deposit({ value: tokenvalue });
+        /*         await manage.deposit({ value: tokenvalue }, { value: tokencol }, { value: tokenloan });
+         */
     });
 });
