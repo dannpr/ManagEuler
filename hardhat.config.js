b99e5745b0878a04fea7ff50fcd2a90a67140702
danny ba <dannba0@gmail.com>
2022-10-29 14:58:49 +0100

    .env added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
-const ALCHEMY_API_KEY = "y9Hwv9sPpVTdg653ZwbBEJQTiwHTmCQw";
-const GOERLI_PRIVATE_KEY = "aed18517b02566597ac057394f9ab24468c5f31215030ac25df06448db7d909e";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,13 +10,13 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
     }
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: '7F9FANI79KMS5W817Z41IXA12NIT1UWPKE',
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
